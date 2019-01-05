507bf025e39f8217816e556eb02559aa93617293
bsreeram08 <bsreeram08@gmail.com>
2019-01-05 23:15:07 +0530

    Android Repository Change

---
+++ New folder/Loginsave/gen/com/example/loginsave/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.loginsave;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- New folder/loginlinearlayout/gen/com/sreeram/loginlinearlayout/R.java
+++ New folder/loginlinearlayout/gen/com/sreeram/loginlinearlayout/R.java
@@ -11,12 +11,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int button2=0x7f020000;
+        public static final int button_bg=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int layout_2=0x7f020003;
+        public static final int shape=0x7f020004;
+        public static final int signup=0x7f020005;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f070000;
         public static final int LinearLayout1_1=0x7f070001;
-        public static final int menu_settings=0x7f070002;
+        public static final int email=0x7f070005;
+        public static final int menu_settings=0x7f07000c;
+        public static final int password=0x7f070007;
+        public static final int signup=0x7f070009;
+        public static final int signupfb=0x7f07000b;
+        public static final int signupimage=0x7f070002;
+        public static final int username=0x7f070003;
+        public static final int view1=0x7f070004;
+        public static final int view2=0x7f070006;
+        public static final int view3=0x7f070008;
+        public static final int view4=0x7f07000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
